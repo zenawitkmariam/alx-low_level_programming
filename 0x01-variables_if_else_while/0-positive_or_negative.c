1eacb7c79e6668cda1c5fafebd8a817a91d8226d
zenawitkmariam <79081895+zenawitkmariam@users.noreply.github.com>
2022-09-09 22:29:38 +0300

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,8 @@
-if (n>0)
-{
+if(n>0) {
         printf("%d: is postive,n\n");
-}
-else if (n<0)
-{
+} else if(n<0) {
         printf("%d: is negative,n\n");
-}
-else
-{
+} else {
         printf("%d: is zero,n\n");
 }
 return (0);
