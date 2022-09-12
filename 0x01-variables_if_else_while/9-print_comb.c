de56dc505b7f424ac7f9f54ca5e8d6e297eba70a
zenawitkmariam <79081895+zenawitkmariam@users.noreply.github.com>
2022-09-12 11:23:44 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,12 +12,12 @@ int main(void)
 	
 	for (i = '0'; i <= '9' ; i++)
 	{
-		putchar(i);
-		if (i != '9')
-		{
-			putchar(',');
-			putchar(' ');
-		}
+	putchar(i);
+	if (i != '9')
+	{
+		putchar(',');
+		putchar(' ');
+	}
 	}
 	putchar('\n');
 	return (0);
