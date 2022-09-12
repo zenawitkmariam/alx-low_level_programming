ef5a49a99035d87eb7cce7061d83a88b9503bee8
zenawitkmariam <79081895+zenawitkmariam@users.noreply.github.com>
2022-09-12 11:20:21 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,7 +2,6 @@
 #include <unistd.h>
 /**
 * main - print the string in the put function
-*
 * Description: using the main function
 * this program prints "Programming is like building a multilingual puzzle
 * Return: 0
