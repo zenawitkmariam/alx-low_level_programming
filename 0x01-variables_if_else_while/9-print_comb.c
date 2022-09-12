e085f5f72e034d841c58e9f4615cb810fbc076b1
zenawitkmariam <79081895+zenawitkmariam@users.noreply.github.com>
2022-09-12 10:51:00 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
  * main - Entry point
  * Description: prints numbers
  * Return: Always 0 (success)
  */
-
 int main(void)
 {
 	int i;
 	
 	for (i = '0'; i <= '9' ; i++)
 	{
-		putchar(i);
+	putchar(i);
 
-		if (i != '9')
-		{
-			putchar(',');
-			putchar(' ');
-		}
+	if (i != '9')
+	{
+		putchar(',');
+		putchar(' ');
+	}
 	}
 	putchar('\n');
 	return (0);
