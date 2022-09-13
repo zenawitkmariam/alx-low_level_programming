fe9b2908e6f166f5e1a7b0998c2bb0aa164ddc42
zenawitkmariam <79081895+zenawitkmariam@users.noreply.github.com>
2022-09-13 13:53:41 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,14 +1,24 @@
-#inclide <main.h>
+#include "main.h"
 
 /**
- * main: here is the entry point
- * description: print _putchar()
- * doc: betty-doc
- * return: 0
+ * print_sign - Entr point
+ * @n: carrier variable
+ * Description: prints the sign of a number
+ * Return: 1 if n>0, 0 if n == 0, -1 if n<0
  */
- 
-int main (void)
+
+int print_sign(int n)
 {
-        _putchar("\n");
-        return (0);
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+_putchar('0');
+return (0);
 }
