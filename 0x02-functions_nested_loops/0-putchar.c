e84f230706557ad1cc3cccf5cc2eb36852fe7476
zenawitkmariam <79081895+zenawitkmariam@users.noreply.github.com>
2022-09-13 12:06:20 +0300

    Update 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#inclide <stdin.io>
+
+/**
+ * main: here is the entry point
+ * description: print _putchar()
+ * doc: betty-doc
+ * return: 0
+ */
+ 
+int main (void)
+{
+        printf(_putchar());
+        return (0);
+}
