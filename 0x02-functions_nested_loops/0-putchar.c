343d6fd471b2676e33f1126c4a94cf9dd9dda806
zenawitkmariam <79081895+zenawitkmariam@users.noreply.github.com>
2022-09-13 13:18:28 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,19 @@
-#inclide <stdio.h>
-#inclide <main.h>
-
+#include "main.h"
 /**
- * main: here is the entry point
- * description: print _putchar()
- * doc: betty-doc
- * return: 0
+ * main - Prints _putchar as a message.
+ *
+ * Return: Always 0 (Success)
  */
- 
-int main (void)
+int main(void)
 {
-        _putchar("\n");
-        return (0);
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count, sz;
+
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
+	{
+		_putchar(str[count]);
+	}
+	_putchar('\n');
+	return (0);
 }
