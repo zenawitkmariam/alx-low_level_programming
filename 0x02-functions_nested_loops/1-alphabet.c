91dc3f01df54f09642b2b31b554767dae4454e8e
zenawitkmariam <zenawitkmariam@gmail.com>
2022-09-13 03:48:19 -0700

    all

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#inclide <main.h>
+
+/**
+ * main: here is the entry point
+ * description: print _putchar()
+ * doc: betty-doc
+ * return: 0
+ */
+ 
+int main (void)
+{
+        _putchar("\n");
+        return (0);
+}
