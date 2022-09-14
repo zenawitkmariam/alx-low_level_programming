567e6e575d533e49fb3b2326b0e30d3fa646dc05
zenawitkmariam <79081895+zenawitkmariam@users.noreply.github.com>
2022-09-14 15:36:27 +0300

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,38 +1,13 @@
-#include "main.h"
-<<<<<<< HEAD
-/**
- * main - Prints _putchar as a message.
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	int count, sz;
-
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
-	{
-		_putchar(str[count]);
-	}
-	_putchar('\n');
-	return (0);
-=======
-
-/**
- * add - adds two integers
- * @n1: integer 1
- * @n2: integer 2
- * Description: give two integers, returns sum
- * Return: sum
+s #include "main.h"
+/*
+* main : return sum of two parameters given
+* Description : add two integers
  */
 
-int add(int n1, int n2)
+int main (int n, int m)
 {
-	int sum;
-
-	sum = n1 + n2;
-
+	int sum=0;
+	sum= n + m;
+	
 	return (sum);
->>>>>>> 3fd1ecf0d04b7660d20a9589f7796e700944d966
 }
