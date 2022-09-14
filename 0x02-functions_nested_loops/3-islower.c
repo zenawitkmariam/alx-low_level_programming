f4194b59ce7b16b17ffeb9146f8255c77fe88b76
zenawitkmariam <79081895+zenawitkmariam@users.noreply.github.com>
2022-09-14 12:41:53 +0300

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,20 +3,19 @@
 /**
  * _islower - Entry point
  * Description: checks is a character is lowercase
- * @c: the integer value it receives
  * Return: 1 if true. 0 if false.
  */
 int _islower(int c)
 {
-int i = 'a';
+        int i;
+        char alpha[25] = "abcdefghijklmnopqrstuvwxyz";
 
-for (i = 'a'; i <= 'z'; i++)
-{
-/* refer int c*/
-if (c == i)
-{
-return (1);
-}
-}
-return (0);
+        for (i = 1; i.length <= 25; i++)
+        {
+                if (c == alpha[i])
+                {
+                        return (1);
+                }
+        }
+        return (0);
 }
