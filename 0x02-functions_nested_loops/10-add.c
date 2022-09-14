1a073ce8fafbd51f3e2b713f382d67020590cee7
zenawitkmariam <79081895+zenawitkmariam@users.noreply.github.com>
2022-09-14 15:48:31 +0300

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,13 +1,14 @@
 #include "main.h"
-/*
-* main : return sum of two parameters given
-* Description : add two integers
+
+/**
+ * main : return sum of two parameters given
+ * Description : add two integers
+ * Return : sum
  */
 
 int add(int n, int m)
 {
 	int sum=0;
 	sum = n + m;
-	
 	return (sum);
 }
