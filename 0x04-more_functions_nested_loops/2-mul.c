00d3c31a737de7869be742513d1543a5cc54bf48
zenawitkmariam <79081895+zenawitkmariam@users.noreply.github.com>
2022-09-15 17:11:07 +0300

    Update 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - gives product
+ * @a: first variable to be multiplied by
+ * @b: second variable to be multiplied 
+ * Return: 1 if true, 0 if false
+ */
+
+int mul(int a, int b)
+{
+    int a = 3;
+    int b = 90;
+    int sum;
+    sum = a * b;
+    
+		return (sum);
+}
