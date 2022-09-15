c8c6d1d688895cd06138b99d0d25abc92fd7cf03
zenawitkmariam <79081895+zenawitkmariam@users.noreply.github.com>
2022-09-15 20:10:12 +0300

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -6,14 +6,11 @@
 * Return : 1 if degit 0 if not
 */
 
-int _isdigit(int d)
+int _isdigit(int c)
 {
-	if(d>=0 && d<=9)
-	{
-		return(1);
-	}
-	else 
-	{
-	        return(0);
-	}
+	if (c >= '0' && c <= '9')
+		return (1);
+
+	else
+		return (0);
 }
