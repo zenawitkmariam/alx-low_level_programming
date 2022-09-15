248e6d65dc1434030da9b5dcd6d9273232046c43
zenawitkmariam <79081895+zenawitkmariam@users.noreply.github.com>
2022-09-15 16:11:48 +0300

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-* isupper- tells weather the letter is uppercase or lowercase.
+* _isupper- tells weather the letter is uppercase or lowercase.
 * @l : is the letter to be checked
 * Return : 1 if uppercase 0 if not
 */
 
-int isupper(int l)
+int _isupper(int l)
 {
  int i;
  i=119;
