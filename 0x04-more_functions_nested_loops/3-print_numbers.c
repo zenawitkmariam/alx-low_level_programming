b691083c6b503a713777d2bf8b45d3b4d8579b46
zenawitkmariam <79081895+zenawitkmariam@users.noreply.github.com>
2022-09-15 17:17:22 +0300

    Update 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_numbers - print 0 to 9
+* 
+* Return : nothing
+*/
+void print_numbers(void)
+{
+ int i;
+  
+ for(i=0, i<10,i++)
+ { 
+  _putchar("%d,i");  
+ }
+ _putchar(\n);
+}
